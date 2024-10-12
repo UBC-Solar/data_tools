5ce5a3fbeacf2a85e4c04ed2276e9332ade47107
Joshua Riefman <joshuariefman@gmail.com>
2024-10-12 11:47:11 -0700

    Fix static data path

--- data_tools/collections/fsgp_2024_laps.py
+++ data_tools/collections/fsgp_2024_laps.py
@@ -9,7 +9,7 @@ class FSGPDayLaps:
     def __init__(self, day: int):
         self.day: int = day
 
-        self.filepath: pathlib.Path = (pathlib.Path(__file__).parent.parent.parent
+        self.filepath: pathlib.Path = (pathlib.Path(__file__).parent.parent
                          / "static_data"
                          / "fsgp_timing_2024"
                          / f"fsgp_timing_day_{day}.csv")
